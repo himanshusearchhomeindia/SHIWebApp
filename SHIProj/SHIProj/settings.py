79b6c83a86d5e2a9bcc58ed46e338b100fafc08d
Himanshu's PC <himanshusearchhomeindia@gmail.com>
2021-03-15 12:39:32 +0530

    added db

--- SHIProj/SHIProj/settings.py
+++ SHIProj/SHIProj/settings.py
@@ -86,7 +86,7 @@ WSGI_APPLICATION = 'SHIProj.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'SHIdb',  #database name
+        'NAME': 'shidb',  #database name
         'USER': 'shipostgres',  #username
         'PASSWORD': 'search@123',  #password
         'HOST': 'localhost',  #hostname
