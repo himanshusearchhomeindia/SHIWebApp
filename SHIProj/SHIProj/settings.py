dc12366be759e13f2a3cdbe3d61e0f7c6eda3f0f
Himanshu's PC <himanshusearchhomeindia@gmail.com>
2021-03-19 16:34:16 +0530

    static

--- SHIProj/SHIProj/settings.py
+++ SHIProj/SHIProj/settings.py
@@ -156,7 +156,7 @@ USE_TZ = True
 
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles/')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # STATICFILES_DIRS = [STATIC_DIR, ]
 
 
